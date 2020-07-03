d70cd64ffc3cbeebd77ba1961886182577eda022
hbpark2 <hbpark2@naver.com>
2020-07-03 20:06:13 +0900

    add slider dot

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
         infinite: true,
         dots: true, //페이지버튼
         autoplay: true, //자동실행
-        pauseOnHover: false, //마우스올렸을때 멈춤기능
+        pauseOnHover: true, //마우스올렸을때 멈춤기능
         autoplaySpeed: 3000, //슬라이드간격(시간)
         speed: 300, //애니메이션 속도
         swipe: true, //pc에서는 마우스 드래그이동, 모바일은 손으로 쓸어넘기는 기능
