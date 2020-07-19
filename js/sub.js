9523166c0e8e946c7c9d6e76922b631dec8d5193
hbpark2 <hbpark2@naver.com>
2020-07-19 20:34:28 +0900

    a

--- js/sub.js
+++ js/sub.js
@@ -1,28 +1,17 @@
 const showbox = document.querySelector('.showbox')
 const thumBox = document.querySelectorAll('#content .thum_box')
-const closeBtn = document.querySelector('.close_btn')
+const closeBtn = document.querySelector('.close_btn>i')
 const detailImg = document.querySelectorAll('.detail_img')
 
 showbox.style.height = window.screen.height + 'px'
 
-// thumBox.forEach((el) => {
-//     el.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         showbox.style.zIndex = 100;
-//         showbox.style.opacity = 1;
-
-
-
-
-//     })
-// })
 
 for (let i = 0; i < thumBox.length; i++) {
     thumBox[i].addEventListener('click', (e) => {
         e.preventDefault();
         showbox.style.zIndex = 100;
         showbox.style.opacity = 1;
-
+        closeBtn.style.opacity = 1;
         console.log(detailImg[i])
         detailImg[i].style.display = 'block'
 
